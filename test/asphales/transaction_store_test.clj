09be55a969f2b973709fef68b08a97eb0c1be2a8
Mike Schaeffer <mike@mschaef.com>
2025-06-09 12:42:56 -0400

    Add some additonal transaction store tests.

--- test/asphales/transaction_store_test.clj
+++ test/asphales/transaction_store_test.clj
@@ -1,7 +1,8 @@
 (ns asphales.transaction-store-test
   (:require [clojure.test :refer :all]
             [asphales.coin :refer :all]
-            [asphales.transaction-store :refer :all]))
+            [asphales.transaction-store :refer :all]
+            [asphales.storage :as storage]))
 
 (deftest initial-state
   (testing "Initial transaction store has only the genesis block."
@@ -66,3 +67,73 @@
         (retract-state! tok))
       (with-transaction s
         (is (thrown? RuntimeException (retract-state! tok)))))))
+
+
+(deftest active-state-inspection-by-id
+  (testing "A ledger starts out without any active state"
+    (let [s (init-transaction-store)]
+      (with-transaction s
+        (is (= #{} (active-state-ids))))))
+
+  (testing "Once asserted, a state is listed by active-state-ids"
+    (let [s (init-transaction-store)]
+      (with-transaction s
+        (let [tok (assert-state! {:magic-word "xyzzy"})]
+          (println tok)
+          (is (= #{tok} (active-state-ids)))))))
+
+  (testing "Once asserted, mulitple states are listed by active-state-ids"
+    (let [s (init-transaction-store)]
+      (with-transaction s
+        (let [[tok-1 tok-2 tok-3] [(assert-state! {:n 1})
+                                   (assert-state! {:n 2})
+                                   (assert-state! {:n 3})]]
+          (is (= #{tok-1 tok-2 tok-3} (active-state-ids)))))))
+
+  (testing "After being asserted and retracted within a transaction, a state is not listed by active-state-id"
+    (let [s (init-transaction-store)]
+      (with-transaction s
+        (let [[tok-1 tok-2 tok-3] [(assert-state! {:n 1})
+                                   (assert-state! {:n 2})
+                                   (assert-state! {:n 3})]]
+          (retract-state! tok-2)
+          (is (= #{tok-1 tok-3} (active-state-ids)))
+          (retract-state! tok-1)
+          (retract-state! tok-3)
+          (is (= #{} (active-state-ids)))))))
+
+  (testing "After being asserted and retracted, a state is not listed by active-state-id"
+    (let [s (init-transaction-store)
+          [tok-1 tok-2 tok-3] (with-transaction s
+                                [(assert-state! {:n 1})
+                                 (assert-state! {:n 2})
+                                 (assert-state! {:n 3})])]
+      (with-transaction s
+        (retract-state! tok-2))
+      (with-transaction s
+        (is (= #{tok-1 tok-3} (active-state-ids))))
+      (with-transaction s
+        (retract-state! tok-1)
+        (retract-state! tok-3))
+      (with-transaction s
+        (is (= #{} (active-state-ids)))))))
+
+(deftest version-conflict-detection
+  (testing "Transactions fail if there is a version conflict"
+    (let [s (init-transaction-store)
+          genesis-transaction-tok (storage/get-root s)]
+      (with-transaction s
+        (assert-state! {:n 1}))
+      (is (thrown? RuntimeException
+                   (with-transaction s
+                     ;; Force a version conflict by updating the
+                     ;; storage root to point to some other token.
+                     (storage/update-root s (storage/get-root s) genesis-transaction-tok)))))))
+
+(deftest nested-transaction-detection
+  (testing "Nested transactions are not supported and fail if attempted."
+    (let [s (init-transaction-store)]
+      (with-transaction s
+        (is (thrown? RuntimeException
+                     (with-transaction s
+                       (assert-state! {:n 1}))))))))
